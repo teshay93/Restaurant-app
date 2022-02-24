e27698c2e5a952dd0be1cb71739fb42782e7aa53
Ashley Chavis <ashleychavis@Ashleys-MBP.lan>
2022-02-24 18:05:48 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import Header  from './container/Header/Header';
-import  Navbar from './components/Navbar/Navbar';
+
 import './App.css';
+import  Navbar from './components/Navbar/Navbar';
+import Header  from './container/Header/Header';
 import AboutUs from './container/AboutUs/AboutUs';
+import SpecialMenu from './container/Menu/SpecialMenu';
+import Chef from './container/Chef/Chef';
+import Intro from './container/Intro/Intro';
+import Laurels from './container/Laurels/Laurels';
+import Gallery from './container/Gallery/Gallery';
+import FindUs from './container/Findus/FindUs';
+import Footer from './container/Footer/Footer';
 
 export const App = () => {
   return (
@@ -10,6 +18,13 @@ export const App = () => {
         <Navbar />
         <Header />
         <AboutUs />
+        <SpecialMenu />
+        <Chef />
+        <Intro />
+        <Laurels />
+        <Gallery />
+        <FindUs />
+        <Footer />
     </div>
   )
 }
